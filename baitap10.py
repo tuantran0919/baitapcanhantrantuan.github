186a3fa99374e320b3a3309700b1954b4e7932dd
tuantran0919 <trantuan2006123456ahihingungok@gmail.com>
2026-04-02 08:32:05 +0700

    baitap10.py
    Added a function to validate SIN numbers and updated input prompts.

--- baitap10.py
+++ baitap10.py
@@ -1,15 +1,17 @@
+def check_sin(s):
+    if len(s) != 9 or not s.isdigit():
+        return False
+    d = list(map(int, s[::-1]))
+    tong = d[0]  # số kiểm tra
+    for i in range(1, 9):
+        if i % 2 == 1:
+            x = d[i] * 2
+            tong += x // 10 + x % 10
+        else:
+            tong += d[i]
+    return tong % 10 == 0
 while True:
-    s= input ("SIN (0 de thoat):")
+    s = input("Nhập SIN (0 để thoát): ")
     if s == "0":
-        tong = 0
-        for i in range(9):
-            x = int (s[i])
-            if i % 2 == 1 :
-                x*=2
-                if x > 9:
-                    x = x // 10 + x % 10
-            tong += xrange
-        if tong % 10 == 0:
-            print ("SIN hop le!")
-        else :
-            print ("SIN khong hop le!")
+        break
+    print("Hợp lệ" if check_sin(s) else "Không hợp lệ")
