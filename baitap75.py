6ae9b33c59131f809ddb82892631865163d29667
tuantran0919 <trantuan2006123456ahihingungok@gmail.com>
2026-04-02 11:48:57 +0700

    baitap75.py

--- baitap75.py
+++ baitap75.py
@@ -1,22 +1,30 @@
-# Nhap n
-n = int(input("Nhap so phan tu: "))
+# Nhập số phần tử
+n = int(input("Nhập n: "))
 
-# Nhap mang
+# Nhập mảng
 a = []
 for i in range(n):
     x = int(input(f"a[{i}] = "))
     a.append(x)
 
-# Dem so lan xuat hien
+# Đếm số lần xuất hiện
 count = {}
-
 for x in a:
     if x in count:
         count[x] += 1
     else:
         count[x] = 1
 
-# In ket qua
-print("\nSo lan xuat hien:")
-for k in count:
-    print(k, "xuat hien", count[k], "lan")
+# Tìm phần tử xuất hiện nhiều nhất (lấy cái xuất hiện đầu tiên)
+max_freq = max(count.values())
+for x in a:
+    if count[x] == max_freq:
+        print("Phần tử xuất hiện nhiều nhất:", x)
+        break
+
+# Tìm phần tử xuất hiện ít nhất (lấy cái xuất hiện đầu tiên)
+min_freq = min(count.values())
+for x in a:
+    if count[x] == min_freq:
+        print("Phần tử xuất hiện ít nhất:", x)
+        break
